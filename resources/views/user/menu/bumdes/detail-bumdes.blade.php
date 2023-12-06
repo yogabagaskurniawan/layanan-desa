41adcfd8ba038802f899e876086d8c7544d5ec1a
Lutfiardii <Lutfiardi873@gmail.com>
2023-12-06 07:34:45 +0700

    Bumdes

--- resources/views/user/menu/bumdes/detail-bumdes.blade.php
+++ resources/views/user/menu/bumdes/detail-bumdes.blade.php
@@ -6,19 +6,38 @@
 <div class="pb-2"></div>
 <div class="card card-style">
     <div class="content">
-        {{-- <div class="text-center">
-            <h1 class="">Museum Sultan Mahmud Badaruddin</h1>
-            <p class="m-0">Jam Buka / Tutup : 21:21:00-21:21:00</p>
-            <p>Kota Pekalongan <br> Desa Satu</p>
-        </div> --}}
-        <div class="divider my-3"></div>
-        <div >
-            <p style="margin: 5px 0;">Bumdes9</p>
-            <p style="color: blue;margin: 5px 0;font-size:18px;">Batang</p>
+        <div class="text-center">
+            <h4 style="margin: 5px 0;">Bumdes9</h4>
+            <p style="color: green;margin: 5px 0;">Batang</p>
             <p style="margin: 5px 0;">Desa Satu</p>
-            <p style="color: green;margin: 5px 0;font-size:22px;">Rp 250,000</p>
-            <p style="margin: 5px 0;">Bumdes9</p>
+            <p style="color: blue;margin: 5px 0;font-size:18px;">Rp 250,000</p>
+            {{-- <p style="margin: 5px 0;">Bumdes9</p> --}}
         </div>
+        
+        <div class="divider my-3"></div>
+        <p>
+                By accessing this web site, you are agreeing to be bound by these
+            web site Terms and Conditions of Use, all applicable laws and regulations,
+            and agree that you are responsible for compliance with any applicable local
+            laws. If you do not agree with any of these terms, you are prohibited from
+            using or accessing this site. The materials contained in this web site are
+            protected by applicable copyright and trade mark law.
+        </p>
+        <p>
+                By accessing this web site, you are agreeing to be bound by these
+            web site Terms and Conditions of Use, all applicable laws and regulations,
+            and agree that you are responsible for compliance with any applicable local
+            laws. If you do not agree with any of these terms, you are prohibited from
+            using or accessing this site. The materials contained in this web site are
+            protected by applicable copyright and trade mark law.
+        </p>
+        {{-- <div >
+            <h4 style="margin: 5px 0;">Bumdes9</h4>
+            <p style="color: blue;margin: 5px 0;">Batang</p>
+            <p style="margin: 5px 0;">Desa Satu</p>
+            <p style="color: green;margin: 5px 0;font-size:18px;">Rp 250,000</p>
+            <p style="margin: 5px 0;">Bumdes9</p>
+        </div> --}}
         <div class="divider my-3"></div>
         <h4>Bagikan Produk Bumdes</h4>
         <div class="d-flex">
@@ -41,6 +60,61 @@
                 <a href="">
                     <span class="icon icon-m rounded-s gradient-mint shadow-bg shadow-bg-xs"><i class="bi bi-whatsapp font-20 color-white"></i></span>
                 </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card card-style">
+    <div class="content">
+        <div class="text-start">
+            <h1 class="">Bumdes Lainnya</h1>
+        </div>
+        <div class="divider my-3"></div>
+        <div class="row mb-0 mx-1">
+            <div class="col-6 col-sm-4 col-lg-3 mb-4">
+                <div class="card card-style m-0 rounded-3">
+                    <a href="/detail-bumdes">
+                        <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
+                    </a>
+                    <div class="content pb-0 text-center">
+                        <h2 class="text-truncate" style="max-width: 100%;">bumdes 1</h2>
+                        <p class="mb-0 font-14"><span class="text-primary font-16"> Rp 250.000</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-sm-4 col-lg-3 mb-4">
+                <div class="card card-style m-0 rounded-3">
+                    <a href="/detail-bumdes">
+                        <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
+                    </a>
+                    <div class="content pb-0 text-center">
+                        <h2 class="text-truncate" style="max-width: 100%;">bumdes 2</h2>
+                        <p class="mb-0 font-14"><span class="text-primary font-16"> Rp 250.000</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-sm-4 col-lg-3 mb-4">
+                <div class="card card-style m-0 rounded-3">
+                    <a href="/detail-bumdes">
+                        <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
+                    </a>
+                    <div class="content pb-0 text-center">
+                        <h2 class="text-truncate" style="max-width: 100%;">bumdes 3</h2>
+                        <p class="mb-0 font-14"><span class="text-primary font-16"> Rp 250.000</span></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-sm-4 col-lg-3 mb-4">
+                <div class="card card-style m-0 rounded-3">
+                    <a href="/detail-bumdes">
+                        <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
+                    </a>
+                    <div class="content pb-0 text-center">
+                        <h2 class="text-truncate" style="max-width: 100%;">bumdes 4</h2>
+                        <p class="mb-0 font-14"><span class="text-primary font-16"> Rp 250.000</span></p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
