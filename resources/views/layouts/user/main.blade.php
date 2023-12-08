9ab8c2bda32c3f697003a32a2a1816dabdac0cfa
heruusty <herulibra27@gmail.com>
2023-12-08 19:17:35 +0700

    memperbaiki file main

--- resources/views/layouts/user/main.blade.php
+++ resources/views/layouts/user/main.blade.php
@@ -83,23 +83,6 @@
                         </div>
                     </div> --}}
                     </div>
-        <!-- Page Title-->
-        <div class="pt-3">
-            <div class="page-title d-flex">
-                <div class="align-self-center me-auto">
-                    <p class="color-theme header-date"></p>
-                    <h1 class="color-theme">Desa Satu</h1>
-                </div>
-                <div class="align-self-center ms-auto d-flex">
-                    <a href="#" data-bs-toggle="offcanvas" data-bs-target="#menu-highlights" class="icon bg-white color-theme rounded-m shadow-xl">
-                        <i class="bi bi-palette-fill color-black font-16"></i>
-                    </a>
-                    <a href="#" data-toggle-theme="" class="show-on-theme-dark icon bg-white color-theme rounded-m shadow-xl"><i class="bi bi-sun-fill color-yellow-dark font-16"></i></a>
-                    <a href="#" data-toggle-theme="" class="show-on-theme-light icon bg-white color-theme rounded-m shadow-xl"><i class="bi bi-moon-fill font-13"></i>
-                    <a href="/"
-                    class="icon gradient-blue shadow-bg shadow-bg-s rounded-m">
-                        <img src="{{ asset('user/images/pictures/logo.png') }}" width="45" class="rounded-m" alt="img">
-                    </a>
                 </div>
             </div>
 
