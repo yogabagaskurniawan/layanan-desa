670c47e3302d8d67195e9e4c2e01a72ff6a0bcce
Lutfiardii <Lutfiardi873@gmail.com>
2023-12-09 23:38:03 +0700

    pengaturanSidebar

--- resources/views/user/akun/pengaturanBankSampah/ambilSampah.blade.php
+++ resources/views/user/akun/pengaturanBankSampah/ambilSampah.blade.php
@@ -80,7 +80,41 @@
     </div>
 </div>
 
-<div id="menu-modal-window" style="height: 530px; width: 340px; display: block;" class="offcanvas offcanvas-modal rounded-m ">
+<div id="menu-modal-window" style="height: 480px; width: 300px; display: block;" class="offcanvas offcanvas-modal rounded-m ">
+    <div class="header-bar header-center shadow-m">
+        <h2 class="m-auto">Detail Nasabah</h2>
+        <button class="btn btn-close p-1 ms-auto" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="d-flex m-3">
+        <div class="content mt-0">
+        <div class="align-self-center" style="margin: 0; padding: 0;">
+            <h5>Data Nasabah</h5>
+            <hr>
+            <p style="margin: 0;">Nama Lengkap : Slamet</p>
+            <p style="margin: 0;">RT : 1</p>
+            <p style="margin: 0;">RW : 1</p>
+            <p style="margin: 0;">Alamat : Batang</p>
+            <p style="margin: 0;">Nomor Telephone : 085710850889</p> 
+            <hr>
+            <h5>Data Akun</h5>
+            <hr>
+            <p style="margin: 0;">Username : Bagas</p>
+            <p style="margin: 0;">Nama Lengkap : yoga bagas kurniawan</p>
+            <p style="margin: 0;">Alamat : Pekalongan</p>
+            <p style="margin: 0;">Nomor Telephone : 085176720024</p>
+        </div> 
+        <hr>
+        {{-- <div class="divider my-5"></div> --}}
+        <h3 class="text-center mb-2">Hubungi Petugas</h3>
+        <div class="my-2 d-flex justify-content-center">
+            <a href="/toko-edit" class="btn btn-xxs bg-mint-dark shadow-bg-m text-center mx-1"><i class="bi bi-whatsapp pe-3 ms-n1"></i>Whatsapp </a>
+            <a href="/toko-produkAdd" class="btn btn-xxs bg-red-dark shadow-bg-m text-center mx-1"><i class="bi bi-telephone pe-3 ms-n1"></i>Telphone </a>
+        </div>
+        </div>
+    </div>  
+</div>
+
+{{-- <div id="menu-modal-window" style="height: 530px; width: 340px; display: block;" class="offcanvas offcanvas-modal rounded-m ">
     <div class="header-bar header-center shadow-m">
         <h2 class="m-auto">Detail Nasabah</h2>
         <button class="btn btn-close p-1 ms-auto" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -107,6 +141,6 @@
             <a class="col btn btn-full btn-l btn-3d bg-orange-light border-red-dark text-center"><i class="bi bi-telephone pe-3 ms-n1"></i>Hubungi </a>
         </div>  
     </div>    
-</div>
+</div> --}}
 
 @endsection
--- resources/views/user/akun/pengaturanBankSampah/pendaftaran.blade.php
+++ resources/views/user/akun/pengaturanBankSampah/pendaftaran.blade.php
@@ -78,12 +78,13 @@
     </div>
 </div>
 
-<div id="menu-modal-window" style="height: 560px; width: 340px; display: block;" class="offcanvas offcanvas-modal rounded-m ">
+<div id="menu-modal-window" style="height: 480px; width: 300px; display: block;" class="offcanvas offcanvas-modal rounded-m ">
     <div class="header-bar header-center shadow-m">
         <h2 class="m-auto">Detail Nasabah</h2>
         <button class="btn btn-close p-1 ms-auto" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="d-flex m-3">
+        <div class="content mt-0">
         <div class="align-self-center" style="margin: 0; padding: 0;">
             <h5>Data Nasabah</h5>
             <hr>
@@ -100,12 +101,16 @@
             <p style="margin: 0;">Alamat : Pekalongan</p>
             <p style="margin: 0;">Nomor Telephone : 085176720024</p>
         </div> 
-        <div id="footer-bar"xl>
-            <a class="col btn btn-full btn-l btn-3d bg-mint-light border-green-dark text-center"><i class="bi bi-whatsapp pe-3 ms-n1"></i>Chat </a>
-            <a class="col btn btn-full btn-l btn-3d bg-orange-light border-red-dark text-center"><i class="bi bi-telephone pe-3 ms-n1"></i>Hubungi </a>
-        </div>  
+        <hr>
+        {{-- <div class="divider my-5"></div> --}}
+        <h3 class="text-center mb-2">Hubungi Petugas</h3>
+        <div class="my-2 d-flex justify-content-center">
+            <a href="/toko-edit" class="btn btn-xxs bg-mint-dark shadow-bg-m text-center mx-1"><i class="bi bi-whatsapp pe-3 ms-n1"></i>Whatsapp </a>
+            <a href="/toko-produkAdd" class="btn btn-xxs bg-red-dark shadow-bg-m text-center mx-1"><i class="bi bi-telephone pe-3 ms-n1"></i>Telphone </a>
+        </div>
+        </div>
     </div>
-    <div style="text-align: center;">
+    <div style="text-align: center;margin-bottom: 20px;">
         <button class="btn btn-xxs btn-success" type="submit" onclick="return confirm('konfirmasi pendaftaran?')">Konfirmasi</button>
     </div>    
 </div>
