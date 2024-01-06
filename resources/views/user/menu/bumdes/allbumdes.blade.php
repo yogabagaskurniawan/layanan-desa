485eea00b11a74a140290be4924526f01da9a09f
Lutfiardii <Lutfiardi873@gmail.com>
2024-01-06 12:58:50 +0700

    Bumdes

--- resources/views/user/menu/bumdes/allbumdes.blade.php
+++ resources/views/user/menu/bumdes/allbumdes.blade.php
@@ -1,77 +1,156 @@
 @extends('layouts.user.main')
+@section('header')
+
+<div class="header-bar header-fixed header-app header-auto-show m-auto" style="max-width: 1000px !important;">
+    <a href="/menu"><i class="bi bi-chevron-left font-13"></i></a>
+    <a class="header-title">Marketplace</a>
+    <a href="#" data-toggle-theme class="show-on-theme-dark"><i class="bi bi-sun-fill color-yellow-dark font-16"></i>
+    <a href="#" data-toggle-theme class="show-on-theme-light"><i class="bi bi-moon-fill font-13"></i>
+</div>
+{{-- modal --}}
+<div id="menu-top-detached" style="height: 360px; display: block; max-width: 1000px;" class="offcanvas offcanvas-top rounded-m offcanvas-detached m-auto" aria-modal="true" role="dialog">
+    <div class="d-flex m-3">
+        <div class="align-self-center">
+            <h2 class="font-700 mb-0">Pencarian</h2>
+        </div>
+        <div class="align-self-center ms-auto">
+            <a href="#" class="icon icon-xs me-n2" data-bs-dismiss="offcanvas">
+                <i class="bi bi-x-circle-fill color-red-dark font-16"></i>
+            </a>
+        </div>
+    </div>
+    <div class="content mt-0">
+        <form class="demo-animation needs-validation m-0" novalidate="">
+            <div class="form-custom mb-3 form-floating">
+                <i class="bi bi-person-circle font-14 disabled"></i>
+                <input type="text" class="form-control rounded-xs" id="c1" placeholder="John Doe" pattern="[A-Za-z ]{1,32}" required="">
+                <label for="c1" class="color-theme">Cari Produk</label>
+                <div class="valid-feedback">Excellent!<!-- text for field valid--></div>
+                <div class="invalid-feedback">Name is Missing or Invalid</div>
+                {{-- <span>(required)</span> --}}
+            </div>
+
+            <div class="form-custom mb-3 form-floating">
+                <i class="bi bi-check-circle font-13 disabled"></i>
+                <select class="form-select rounded-xs" id="c6" aria-label="Floating label select example">
+                    <option selected="">Terbaru</option>
+                    <option value="1">Terlama</option>
+                </select>
+                <label for="c6" class="color-theme">Urutkan Berdasarkan</label>
+                <div class="valid-feedback">HTML5 does not offer Dates Field Validation!<!-- text for field valid--></div>
+            </div>
+            <button class="btn btn-full bg-blue-dark rounded-xs text-uppercase font-700 w-100 btn-s mt-4" type="submit">Cari</button>
+        </form>
+    </div>
+</div>   
+@endsection
+
 
 @section('content')
 @include('layouts.user.partials.footer-bar')
 
-<div class="header-bar header-center shadow-m">
-    <h2 class="m-auto">Bumdes</h2>
+<div class="pt-3">
+    <div class="page-title d-flex pb-3">
+        <div class="align-self-center">
+            <a href="/menu" class="me-3 ms-0 icon icon-xxs bg-theme rounded-s shadow-m">
+                <i class="bi bi-chevron-left color-theme font-14"></i>
+            </a>
+        </div>
+        <div class="align-self-center me-auto">
+            <h1 class="color-theme mb-0 font-18">Bumdes</h1>
+        </div>
+    </div>
 </div>
 
 <div class="divider mx-3"></div>
 
-<div class="card card-style">
+<div class=" border-0 mx-0 " style="max-width: 1000px !important;">
     <div class="content mb-0">
         <div class="row">
-            <div class="col-8">
-                <div class="form-custom form-label no-icon mb-3">
-                    <input type="text" class="form-control rounded-xs fs-6" id="c1" placeholder="Cari produk / desa / kota">
-                    <label for="c1" class="color-theme ">Search</label>
+            <div class="row justify-content-between pe-0">
+                <div class="col-10">
+                    <div class="form-custom form-label no-icon mb-3">
+                        <input type="text" class="border border-secondary form-control rounded-pill fs-6 bg-theme" id="c1" placeholder="Cari produk...">
+                        <span class="input-group-text bg-transparent border-0" id="basic-addon2"><i class="bi bi-search font-20"></i></span>
+                        <label for="c1" class="color-theme">Search</label>
+                    </div>
+                    
                 </div>
-            </div>
-            <div class="col-4 form-custom">
-                <select class="form-select rounded-xs" id="c6" aria-label="Floating label select example">
-                    <option selected="">Terbaru</option>
-                    <option value="1">Terlama</option>
-                    <option value="2">Urutkan</option>
-                </select>
+                <div class="col p-0 w-full">
+                    <span data-bs-toggle="offcanvas" data-bs-target="#menu-top-detached" href="#" class="icon icon-xl rounded-pill border border-secondary shadow-bg shadow-bg-xs bg-theme" style="width: 100%; background-color: transparent;">
+                        <i class="bi bi-filter font-32 text-secondary"></i>
+                    </span>
+                </div>  
             </div>
         </div>
-        {{-- <div class="my-2 d-flex justify-content-center">
-            <a href="/tambah-jasa" class="btn btn-xxs bg-mint-dark shadow-bg-m text-center mx-1">Tambah jasa</a>
-            <a href="/jasa-saya" class="btn btn-xxs bg-blue-dark shadow-bg-m text-center mx-1">Jasa Saya</a>
-        </div>         --}}
-        <div class="pb-2"></div>
     </div>
 </div>
 
 <div class="divider mx-3"></div>
 
-<div class="row me-0 ms-0 mb-0">
-    <div class="col-6 col-sm-4 col-lg-3 mb-4">
-        <div class="card card-style m-0 rounded-3">
-            <a href="/detail-bumdes">
-                <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
-            </a>
-            <div class="content pb-0 text-center">
-                <h2 class="text-truncate" style="max-width: 100%;">Bumdes 1</h2>
-                <p class="mb-0 font-14">Batang<br>Desa Satu <br><span class="text-primary font-16"> Rp 250.000</span></p>
+<div class="card card-style mx-0" style="border-radius: 26px !important">
+    <div class="content my-0 px-2">
+        <h3 class="font-200 my-3 ms-n2">Pilihan Terbaik Anda</h3>
+        <div class="row mb-0">
+            <div class="col-4 col-sm-3 col-md-2 px-0">
+                <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                    {{-- <span class="badge bg-primary position-absolute top-0 end-0 m-2 bg-orange-dark px-2 rounded-s font-12">Elektronik</span> --}}
+                    <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                        alt=" card="" image"="">
+                    <div class="content pb-0 mx-2">
+                        <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 1 </p>
+                        <h5 class="text-truncate">Rp 9.000.000</h5>
+                        <p class="card-text">DesaSatu</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-4 col-sm-3 col-md-2 px-0">
+                <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                    {{-- <span class="badge bg-primary position-absolute top-0 end-0 m-2 bg-orange-dark px-2 rounded-s font-12">Makanan</span> --}}
+                    <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                        alt=" card="" image"="">
+                    <div class="content pb-0 mx-2">
+                        <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 2</p>
+                        <h5 class="text-truncate">Rp 9.000.000</h5>
+                        <p class="card-text">DesaSatu</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-4 col-sm-3 col-md-2 px-0">
+                <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                    <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                        alt=" card="" image"="">
+                    <div class="content pb-0 mx-2">
+                        <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 3</p>
+                        <h5 class="text-truncate">Rp 9.000.000</h5>
+                        <p class="card-text">DesaSatu</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-4 col-sm-3 col-md-2 px-0">
+                <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                    <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                        alt=" card="" image"="">
+                    <div class="content pb-0 mx-2">
+                        <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 4</p>
+                        <h5 class="text-truncate">Rp 9.000.000</h5>
+                        <p class="card-text">DesaSatu</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-4 col-sm-3 col-md-2 px-0">
+                <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                    <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                        alt=" card="" image"="">
+                    <div class="content pb-0 mx-2">
+                        <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 5</p>
+                        <h5 class="text-truncate">RP.90.000.000</h5>
+                        <p class="card-text">DesaSatu</p>
+                    </div>
+                </a>
             </div>
         </div>
-    </div>
-
-    <div class="col-6 col-sm-4 col-lg-3 mb-4">
-        <div class="card card-style m-0 rounded-3">
-            <a href="/detail-bumdes">
-                <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
-            </a>
-            <div class="content pb-0 text-center">
-                <h2 class="text-truncate" style="max-width: 100%;">Bumdes 2</h2>
-                <p class="mb-0 font-14">Batang<br>Desa Satu <br><span class="text-primary font-16"> Rp 250.000</span></p>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-6 col-sm-4 col-lg-3 mb-4">
-        <div class="card card-style m-0 rounded-3">
-            <a href="/detail-bumdes">
-                <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
-            </a>
-            <div class="content pb-0 text-center">
-                <h2 class="text-truncate" style="max-width: 100%;">Bumdes 3</h2>
-                <p class="mb-0 font-14">Batang<br>Desa Satu <br><span class="text-primary font-16"> Rp 250.000</span></p>
-            </div>
-        </div>
-    </div>
+    </div> 
 </div>
 
 @endsection
--- resources/views/user/menu/bumdes/detail-bumdes.blade.php
+++ resources/views/user/menu/bumdes/detail-bumdes.blade.php
@@ -2,121 +2,176 @@
 
 @section('content')
 @include('layouts.user.partials.footer-btn')
-<div class="card card-style bg-5 shadow-card shadow-card-l" style="height:300px;background-image: url('{{ asset('user/images/pictures/23.jpg') }}')"></div>
-<div class="pb-2"></div>
-<div class="card card-style">
-    <div class="content">
-        <div class="text-center">
-            <h4 style="margin: 5px 0;">Bumdes9</h4>
-            <p style="color: green;margin: 5px 0;">Batang</p>
-            <p style="margin: 5px 0;">Desa Satu</p>
-            <p style="color: blue;margin: 5px 0;font-size:18px;">Rp 250,000</p>
-            {{-- <p style="margin: 5px 0;">Bumdes9</p> --}}
-        </div>
-        
-        <div class="divider my-3"></div>
-        <p>
-                By accessing this web site, you are agreeing to be bound by these
-            web site Terms and Conditions of Use, all applicable laws and regulations,
-            and agree that you are responsible for compliance with any applicable local
-            laws. If you do not agree with any of these terms, you are prohibited from
-            using or accessing this site. The materials contained in this web site are
-            protected by applicable copyright and trade mark law.
-        </p>
-        <p>
-                By accessing this web site, you are agreeing to be bound by these
-            web site Terms and Conditions of Use, all applicable laws and regulations,
-            and agree that you are responsible for compliance with any applicable local
-            laws. If you do not agree with any of these terms, you are prohibited from
-            using or accessing this site. The materials contained in this web site are
-            protected by applicable copyright and trade mark law.
-        </p>
-        {{-- <div >
-            <h4 style="margin: 5px 0;">Bumdes9</h4>
-            <p style="color: blue;margin: 5px 0;">Batang</p>
-            <p style="margin: 5px 0;">Desa Satu</p>
-            <p style="color: green;margin: 5px 0;font-size:18px;">Rp 250,000</p>
-            <p style="margin: 5px 0;">Bumdes9</p>
-        </div> --}}
-        <div class="divider my-3"></div>
-        <h4>Bagikan Produk Bumdes</h4>
-        <div class="d-flex">
-            <div class="align-self-center me-2">
-                <a href="">
-                    <span class="icon icon-m rounded-s gradient-blue shadow-bg shadow-bg-xs"><i class="bi bi-facebook font-20 color-white"></i></span>
-                </a>
-            </div>
-            <div class="align-self-center me-2">
-                <a href="">
-                    <span class="icon icon-m rounded-s gradient-teal shadow-bg shadow-bg-xs"><i class="bi bi-twitter font-20 color-white"></i></span>
-                </a>
-            </div>
-            <div class="align-self-center me-2">
-                <a href="">
-                    <span class="icon icon-m rounded-s gradient-blue shadow-bg shadow-bg-xs"><i class="bi bi-telegram font-20 color-white"></i></span>
-                </a>
-            </div>
-            <div class="align-self-center me-2">
-                <a href="">
-                    <span class="icon icon-m rounded-s gradient-mint shadow-bg shadow-bg-xs"><i class="bi bi-whatsapp font-20 color-white"></i></span>
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card card-style">
-    <div class="content">
-        <div class="text-start">
-            <h1 class="">Bumdes Lainnya</h1>
-        </div>
-        <div class="divider my-3"></div>
-        <div class="row mb-0 mx-1">
-            <div class="col-6 col-sm-4 col-lg-3 mb-4">
-                <div class="card card-style m-0 rounded-3">
-                    <a href="/detail-bumdes">
-                        <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
-                    </a>
-                    <div class="content pb-0 text-center">
-                        <h2 class="text-truncate" style="max-width: 100%;">bumdes 1</h2>
-                        <p class="mb-0 font-14"><span class="text-primary font-16"> Rp 250.000</span></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-lg-3 mb-4">
-                <div class="card card-style m-0 rounded-3">
-                    <a href="/detail-bumdes">
-                        <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
-                    </a>
-                    <div class="content pb-0 text-center">
-                        <h2 class="text-truncate" style="max-width: 100%;">bumdes 2</h2>
-                        <p class="mb-0 font-14"><span class="text-primary font-16"> Rp 250.000</span></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-lg-3 mb-4">
-                <div class="card card-style m-0 rounded-3">
-                    <a href="/detail-bumdes">
-                        <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
-                    </a>
-                    <div class="content pb-0 text-center">
-                        <h2 class="text-truncate" style="max-width: 100%;">bumdes 3</h2>
-                        <p class="mb-0 font-14"><span class="text-primary font-16"> Rp 250.000</span></p>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col-6 col-sm-4 col-lg-3 mb-4">
-                <div class="card card-style m-0 rounded-3">
-                    <a href="/detail-bumdes">
-                        <img src="{{ asset('user/images/pictures/24.jpg')}}" class="img-fluid">
-                    </a>
-                    <div class="content pb-0 text-center">
-                        <h2 class="text-truncate" style="max-width: 100%;">bumdes 4</h2>
-                        <p class="mb-0 font-14"><span class="text-primary font-16"> Rp 250.000</span></p>
+<div class="pt-3 sticky-nav">
+    <div class="page-title pb-3 d-flex align-items-center mx-0" style="max-width: 1000px !important;">
+        <div class="me-auto align-self-center">
+            <a href="/bumdes" class="me-3 ms-0 icon icon-xs bg-theme rounded-s shadow-m">
+                <i class="bi bi-chevron-left color-theme font-14"></i>
+            </a>
+        </div>
+        <div class="align-self-center">
+            <a href="#" data-bs-toggle="dropdown" class="me-3 ms-0 icon icon-xs bg-theme rounded-s shadow-m">
+                <i class="bbi bi-share-fill color-theme font-14"></i>
+            </a>
+            <!-- Page Title Dropdown Menu-->
+            <div class="dropdown-menu">
+                <div class="card card-style shadow-m mt-1 me-1">
+                    <div class="list-group list-custom list-group-s list-group-flush rounded-xs px-3 py-1">
+                        <a href="" class="list-group-item">
+                            <i class="has-bg gradient-blue shadow-bg shadow-bg-xs color-white rounded-xs bi bi-facebook"></i>
+                            <strong class="font-13">Facebook</strong>
+                        </a>
+                        <a href="" class="list-group-item">
+                            <i class="has-bg gradient-teal shadow-bg shadow-bg-xs color-white rounded-xs bi bi-twitter"></i>
+                            <strong class="font-13">Twitter</strong>
+                        </a>
+                        <a href="" class="list-group-item">
+                            <i class="has-bg gradient-blue shadow-bg shadow-bg-xs color-white rounded-xs bi bi-telegram"></i>
+                            <strong class="font-13">Telegram</strong>
+                        </a>
+                        <a href="" class="list-group-item">
+                            <i class="has-bg gradient-mint shadow-bg shadow-bg-xs color-white rounded-xs bi bi-whatsapp"></i>
+                            <strong class="font-13">Whatsapp</strong>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="card card-style mx-0 rounded-0" style="border-bottom-right-radius: 26px !important;border-bottom-left-radius: 26px !important">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('user/images/bumdes.jpg')}}" class="img-fluid d-block m-auto" style="width: 432px; height: 432px; border-bottom-right-radius: 26px;border-bottom-left-radius: 26px">
+            </div>
+            <div class="carousel-item ">
+                <img src="{{ asset('user/images/pictures/merkLaptop.jpeg') }}" class="img-fluid d-block m-auto" style="width: 432px; height: 432px; border-bottom-right-radius: 26px;border-bottom-left-radius: 26px">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev" style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.8));">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next" style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.8));">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    
+    <div class="content pb-0 mt-3 mb-0">
+        {{-- <div class="d-flex justify-content-between align-items-center mb-3">
+            <div class="pt-1">
+                <img src="{{ asset('user/images/desasatu.png')}}" class="img-fluid rounded-circle" width="80">
+            </div>
+            <div class="ps-3 me-auto">
+                <h2 class="mt-n2">Desa Satu</h2>
+                <p class="card-text font-14">Batang</p>
+            </div>
+        </div> --}}
+        <h2 class="font-200 mb-0">Bumdes 1</h2>
+        <h3 class="font-200 my-3">RP. 9.000.000</h3>
+        <h3 class="font-200">Tentang Bumdes</h3>
+        <p class="mb-3 font-500 font-14">
+            By accessing this web site, you are agreeing to be bound by these
+            web site Terms and Conditions of Use, all applicable laws and regulations,
+            and agree that you are responsible for compliance with any applicable local
+            laws. If you do not agree with any of these terms, you are prohibited from
+            using or accessing this site. The materials contained in this web site are
+            protected by applicable copyright and trade mark law.
+        </p>
+        <hr>
+        <p class="mb-3 font-500 font-14">
+            By accessing this web site, you are agreeing to be bound by these
+            web site Terms and Conditions of Use, all applicable laws and regulations,
+            and agree that you are responsible for compliance with any applicable local
+            laws. If you do not agree with any of these terms, you are prohibited from
+            using or accessing this site. The materials contained in this web site are
+            protected by applicable copyright and trade mark law.
+        </p>
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <div class="pt-1">
+                <img src="{{ asset('user/images/desasatu.png')}}" class="img-fluid rounded-circle" width="80">
+            </div>
+            <div class="ps-3 me-auto">
+                <h2 class="mt-n2">Desa Satu</h2>
+                <p class="card-text font-14">Tulis, Batang</p>
+            </div>
+        </div>
+        {{-- <div class="mb-3 d-flex justify-content-center">
+            <a href="/marketplace/detail-produk/kunjungi-toko" class="btn btn-xxs bg-blue-dark shadow-bg-m text-center mx-1">Kunjungi Toko</a>
+            <a href="/kurir" class="btn btn-xxs bg-mint-dark shadow-bg-m text-center mx-1">Hubungi Kurir</a>
+        </div> --}}
+    </div>
+</div>
+
+
+
+
+<div class="content px-2">
+    <h3 class="font-200 mb-3 ms-n2">Bumdes lain di Toko Ini</h3>
+    <div class="row mb-0">
+        <div class="col-4 col-sm-3 col-md-2 px-0">
+            <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                {{-- <span class="badge bg-primary position-absolute top-0 end-0 m-2 bg-orange-dark px-2 rounded-s font-12">Elektronik</span> --}}
+                <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                    alt=" card="" image"="">
+                <div class="content pb-0 mx-2">
+                    <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 1 </p>
+                    <h5 class="text-truncate">Rp 9.000.000</h5>
+                    <p class="card-text">DesaSatu</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-4 col-sm-3 col-md-2 px-0">
+            <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                {{-- <span class="badge bg-primary position-absolute top-0 end-0 m-2 bg-orange-dark px-2 rounded-s font-12">Makanan</span> --}}
+                <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                    alt=" card="" image"="">
+                <div class="content pb-0 mx-2">
+                    <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 2</p>
+                    <h5 class="text-truncate">Rp 9.000.000</h5>
+                    <p class="card-text">DesaSatu</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-4 col-sm-3 col-md-2 px-0">
+            <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                    alt=" card="" image"="">
+                <div class="content pb-0 mx-2">
+                    <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 3</p>
+                    <h5 class="text-truncate">Rp 9.000.000</h5>
+                    <p class="card-text">DesaSatu</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-4 col-sm-3 col-md-2 px-0">
+            <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                    alt=" card="" image"="">
+                <div class="content pb-0 mx-2">
+                    <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 4</p>
+                    <h5 class="text-truncate">Rp 9.000.000</h5>
+                    <p class="card-text">DesaSatu</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-4 col-sm-3 col-md-2 px-0">
+            <a href="/detail-bumdes" class="card card-style mx-1 mb-2" style="border-radius: 12px !important">
+                <img src="{{ asset('user/images/bumdes.jpg')}}" class="card-img-top img-fluid rounded" style="height: 113px !important;
+                    alt=" card="" image"="">
+                <div class="content pb-0 mx-2">
+                    <p class="font-14 font-900 mb-0 text-truncate" href="/detail-bumdes" style="max-width: 150px;">Bumdes 5</p>
+                    <h5 class="text-truncate">RP.90.000.000</h5>
+                    <p class="card-text">DesaSatu</p>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+
+
 @endsection
