edd734a4d85cb73d3ddc2ef441cf5585cfd5ce48
Lutfiardii <Lutfiardi873@gmail.com>
2024-01-14 18:12:17 +0700

    buatloker

--- resources/views/user/menu/loker/buat-loker.blade.php
+++ resources/views/user/menu/loker/buat-loker.blade.php
@@ -31,44 +31,46 @@
     </div>
 </div>
 
-<div class="card card-style">
-    <div class="content">
-        <form class="demo-animation needs-validation m-0" novalidate="">
-            <div class="form-custom mb-3 form-border form-icon form-label">
-                <i class="bi bi-person-circle font-14"></i>
-                <input type="text" class="form-control rounded-xs" id="c1" placeholder="Masukan nama Loker" pattern="[A-Za-z ]{1,32}" required="">
-                <label for="c1" class="color-theme">Nama Loker</label>
-                <div class="valid-feedback">Excellent!<!-- text for field valid--></div>
-                <div class="invalid-feedback">Name is Missing or Invalid</div>
-                <span>(required)</span>
-            </div>
+<div class="card card-style mx-0 mt-3 pt-3">
+    <div class="container px-3 mx-auto" style="max-width: 576px">
+        <div class="content">
+            <form class="demo-animation needs-validation m-0" novalidate="">
+                <div class="form-custom mb-3 form-border form-icon form-label">
+                    <i class="bi bi-person-circle font-14"></i>
+                    <input type="text" class="form-control rounded-xs" id="c1" placeholder="Masukan nama Loker" pattern="[A-Za-z ]{1,32}" required="">
+                    <label for="c1" class="color-theme">Nama Loker</label>
+                    <div class="valid-feedback">Excellent!<!-- text for field valid--></div>
+                    <div class="invalid-feedback">Name is Missing or Invalid</div>
+                    <span>(required)</span>
+                </div>
+                
+                <div class="form-custom mb-3 form-border form-icon form-label">
+                    <i class="bi bi-window-sidebar font-14"></i>
+                    <textarea class="form-control rounded-xs" placeholder="deskripsi" id="c7"></textarea>
+                    <label for="c7" class="color-theme">Deskripsi</label>
+                    <div class="valid-feedback">HTML5 does not offer Dates Field Validation!<!-- text for field valid--></div>
+                </div>
 
-            <div class="form-custom mb-3 form-border form-icon form-label">
-                <i class="bi bi-window-sidebar font-14"></i>
-                <textarea class="form-control rounded-xs" placeholder="Descripsi yang sesuai" id="c7"></textarea>
-                <label for="c7" class="color-theme">Your Description</label>
-                <div class="valid-feedback">HTML5 does not offer Dates Field Validation!<!-- text for field valid--></div>
-            </div>
-            
-            <div class="form-custom mb-3 form-border form-icon form-label">
-                <i class="bi bi-telephone-fill font-12"></i>
-                <input type="tel" class="form-control rounded-xs" id="c3" placeholder="no telpon" pattern="[+ 0-9]{10,15}" required="">
-                <label for="c3" class="color-theme">Your Phone</label>
-                <div class="valid-feedback">Phone Number looks good!<!-- text for field valid--></div>
-                <div class="invalid-feedback">Phone Number is missing or is invalid.</div>
-                <span>(required)</span>
-            </div>
-            
-            <div class="text-center">
-                <input type="file" class="upload-file" accept="image/*">
-                <p class="btn btn-xxs text-uppercase rounded-s upload-file-text mb-0 border-blue-dark color-blue-dark">Upload gambar</p>
-                <p style="font-size:12px">Maksimal ukuran foto 3MB</p>
-            </div>
-            
-            <button class="btn btn-full bg-mint-dark rounded-xs text-uppercase font-700 w-100 btn-s mt-4" type="submit">Daftar</button>
-        </form>
+                <div class="form-custom mb-3 form-border form-icon form-label">
+                    <i class="bi bi-telephone-fill font-12"></i>
+                    <input type="tel" class="form-control rounded-xs" id="c3" placeholder="no telpon" pattern="[+ 0-9]{10,15}" required="">
+                    <label for="c3" class="color-theme">Your Phone</label>
+                    <div class="valid-feedback">Phone Number looks good!<!-- text for field valid--></div>
+                    <div class="invalid-feedback">Phone Number is missing or is invalid.</div>
+                    <span>(required)</span>
+                </div>
+                <div class="file-data">
+                    <div class="row text-center images-container"></div>
+                    <div class="text-center">
+                        <input type="file" class="upload-file" accept="image/*" multiple>
+                        <p class="btn btn-xxs text-uppercase rounded-s upload-file-text mb-0 border-blue-dark color-blue-dark">Upload gambar</p>
+                        <p style="font-size:12px">Maksimal ukuran foto 3MB Maksimal 3 foto</p>
+                    </div>
+                </div>           
+                <button class="btn btn-full bg-mint-dark rounded-xs text-uppercase font-700 w-100 btn-s mt-4" type="submit">Daftar</button>
+            </form>
+        </div>
     </div>
 </div>
-
 
 @endsection
